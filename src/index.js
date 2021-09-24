72e537b775a0bdbb827e9e9bb9552420687540c4
Jack-Chou-HZ <better.product.go@gmail.com>
2021-09-24 14:35:58 +0800

    add react libraries via putting script tag *before* the require.js script tag

--- src/index.js
+++ src/index.js
@@ -8,15 +8,55 @@
 //   A sample code to use RequireJS to write modular JavaScript code and make
 // use of the NodeJS modules at the same time.
 
-// since the require.js file is loaded in the HTML <script> tag, we don't
+//   Since the require.js file is loaded in the HTML <script> tag, we don't
 // need to define it here again
 // const requirejs = require('requirejs')
 
-requirejs(['./modules/robert'],
+// config the requirejs function
+require.config({
+  baseUrl: './src', // relative to the index.html file path
+  paths: {
+    modules: 'modules', // relative to the baseUrl
+    libs: '../libs'
+  },
+  waitSeconds: 15
+})
+
+//   This is a function call, *not a function definition, nor a self-invoking function*,
+// the requirejs function is called explicitly.
+//   The requirejs function is defined into the global scope
+// after the ./libs/require.js script is loaded in the <script> tag in
+// index.html.
+requirejs(['modules/robert'],
   function (theRobert) {
-    // foo and bar are loaded according to requirejs
-    // config, but if not found, then node's require
-    // is used to load the module.
+    // the JS file ./modules/robert.js is loaded (as a module) according to
+    // requirejs configuration,
     theRobert.sayHello('Lebron James', 'paragraph')
     window.document.title = 'hello world!'
+
+    // this points to the window object which is provided by the host environment
+    // in this case the DOM API by the browser
+    console.log(this)
+    console.log('\n')
+    console.log(window)
+    console.log('\n')
+
+    // check if the react core libraries are loaded
+    /*
+    console.log(React)
+    console.log('\n')
+    console.log(reactDom)
+    */
   })
+
+// Print the define and requirejs function (which is also defined by the
+// ./libs/require.js script)
+
+console.log('define: \n')
+console.log(define)
+console.log('\nrequirejs: \n')
+console.log(requirejs)
+console.log('\nReact:\n')
+console.log(React)
+
+// Use React functions here
