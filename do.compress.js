a0658579d9747e9e8ca55728611d19522c97e2b3
Jack-Chou-HZ <better.product.go@gmail.com>
2021-09-28 17:03:07 +0800

    a config file for r.js to compress the project especially for js files

---
+++ do.compress.js
@@ -0,0 +1,13 @@
+({
+  baseUrl: './src/libs',
+  paths: {
+    modules: '../modules',
+    model: '../model',
+    controllers: '../controllers',
+    views: '../views',
+    configs: '../configs',
+    external: './'
+  },
+  dir: 'dist'
+  }
+)
