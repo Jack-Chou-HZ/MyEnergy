7b0df6b020560ab1fd52a8a7dc07bc743787da1e
Jack-Chou-HZ <better.product.go@gmail.com>
2021-10-07 14:42:48 +0800

    add .yarn directory

--- src/views/about.js
+++ src/views/about.js
@@ -1,3 +1,3 @@
 define(['react'], function (React) {
-  return /*#__PURE__*/React.createElement("p", null, "Welcome to My Energy App");
-});
+  return /* #__PURE__ */React.createElement('p', null, 'Welcome to My Energy App')
+})
--- src/views/index.js
+++ src/views/index.js
@@ -8,32 +8,31 @@
 //   This is a Clock component
 define(['react', 'react-dom'], function (React, ReactDOM) {
   class Clock extends React.Component {
-    constructor(props) {
-      super(props);
+    constructor (props) {
+      super(props)
       this.state = {
         date: new Date()
-      }; // this.tick = this.tick.bind(this)
+      } // this.tick = this.tick.bind(this)
     }
 
-    componentDidMount() {
-      this.timerID = setInterval(() => this.tick(), 1000);
+    componentDidMount () {
+      this.timerID = setInterval(() => this.tick(), 1000)
     }
 
-    componentWillUnmount() {
-      clearInterval(this.timerID);
+    componentWillUnmount () {
+      clearInterval(this.timerID)
     }
 
-    tick() {
+    tick () {
       this.setState({
         date: new Date()
-      });
+      })
     }
 
-    render() {
-      return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Welcome to MyEnergy App!"), /*#__PURE__*/React.createElement("h2", null, "It is ", this.state.date.toLocaleTimeString(), "."));
+    render () {
+      return /* #__PURE__ */React.createElement('div', null, /* #__PURE__ */React.createElement('h1', null, 'Welcome to MyEnergy App!'), /* #__PURE__ */React.createElement('h2', null, 'It is ', this.state.date.toLocaleTimeString(), '.'))
     }
-
   }
 
-  return /*#__PURE__*/React.createElement(Clock, null);
-});
+  return /* #__PURE__ */React.createElement(Clock, null)
+})
