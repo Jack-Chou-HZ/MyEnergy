a698fcee3d6397bbc40cca4b85ce76b97133929d
Jack <better.product.go@gmail.com>
2021-10-12 23:56:34 +0800

    able to display default questions

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -14,10 +14,7 @@ define(['react', 'react-dom', 'antd'], function (React, ReactDOM, AntD) {
     constructor (props) {
       super(props)
       this.getProfile = this.getProfile.bind(this)
-      this.getHardCodedProfile = this.getHardCodedProfile.bind(this)
       this.onTabsChangedHandler = this.onTabsChangedHandler.bind(this)
-
-      this.state = { sleep: '', energy: '', happiness: '' }
 
       this.handleChange = this.handleChange.bind(this)
       this.handleSubmit = this.handleSubmit.bind(this)
@@ -53,118 +50,32 @@ define(['react', 'react-dom', 'antd'], function (React, ReactDOM, AntD) {
     getProfile () {
       // set state
       const { userId } = this.props
+      const { Tabs } = AntD
+      const { TabPane } = Tabs
 
       if (!userId) {
         const { defaultQuestions } = this.props
         if (defaultQuestions) {
-          return (<div>
-            {defaultQuestions.map((value, index) => (
-              <div className='question' key={index}>
-                <div className='ask'><label>{value}</label></div>
-                <div className='answer'><input type="text" name='sleep' value={this.state.sleep} onChange={this.handleChange} /></div>
-              </div>))}
-            </div>)
+          return (
+            <form onSubmit={this.handleSubmit} className='profile'>
+              <Tabs defaultActiveKey="1" onChange={this.onTabsChangedHandler}>
+                <TabPane tab="Physical status" key="1">
+                  {defaultQuestions.map((value, index) => (
+                    <div className='question' key={index}>
+                      <div className='ask'><label>{value.question}</label></div>
+                      <div className='answer'><input type="text" name='sleep' value={this.state.sleep} onChange={this.handleChange} /></div>
+                    </div>))}
+                </TabPane>
+              </Tabs>
+              <div className='button'>
+                <input type="submit" value="Save" />
+                <input type="button" value="Never mind" onClick={this.handleSubmit} />
+              </div>
+           </form>)
         } else {
           return (<h2>Loading initial questions...</h2>)
         }
-
-        // return (
-        //   <form onSubmit={this.handleSubmit} className='profile'>
-        //     <Tabs defaultActiveKey="1" onChange={this.onTabsChangedHandler}>
-        //       <TabPane tab="Physical status" key="1">
-        //           {defaultQuestions.map((value, index) => (
-        //             <div className='question' key={index}>
-        //           <div className='ask'><label>{value}</label></div>
-        //           <div className='answer'><input type="text" name='sleep' value={this.state.sleep} onChange={this.handleChange} /></div>
-        //         </div>
-        //           ))}
-        //       </TabPane>
-        //       <TabPane tab="Emotion status" key="2">
-        //         <div className='question'>
-        //           <label>Did you sleep well last night? <input type="text" name='sleep' value={this.state.sleep} onChange={this.handleChange} /></label>
-        //         </div>
-        //         <div className='question'>
-        //           <label>Do you feel energetic? <input type="text" name='energy' value={this.state.energy} onChange={this.handleChange} /></label>
-        //         </div>
-        //       </TabPane>
-        //       <TabPane tab="Spitial status" key="3">
-        //         <div className='question'>
-        //           <div className='ask'>
-        //             <label>Did you sleep well last night?</label>
-        //           </div>
-        //           <div className='answer'>
-        //             <input type="text" name='sleep' value={this.state.sleep} onChange={this.handleChange} />
-        //           </div>
-        //         </div>
-        //         <div className='question'>
-        //           <label>Do you feel energetic? <input type="text" name='energy' value={this.state.energy} onChange={this.handleChange} /></label>
-        //         </div>
-        //       </TabPane>
-        //     </Tabs>
-        //     <div className='button'>
-        //       <input type="submit" value="Save" />
-        //       <input type="button" value="Never mind" onClick={this.handleSubmit} />
-        //     </div>
-        //   </form>
-        // )
       }
-    }
-
-    // hard code the profile
-    getHardCodedProfile () {
-      const { Tabs } = AntD
-      const { TabPane } = Tabs
-
-      return (
-        <form onSubmit={this.handleSubmit} className='profile'>
-          <Tabs defaultActiveKey="1" onChange={this.onTabsChangedHandler}>
-            <TabPane tab="Physical status" key="1">
-              <div className='question'>
-                <div className='ask'><label>Did you sleep well last night?</label></div>
-                <div className='answer'><input type="text" name='sleep' value={this.state.sleep} onChange={this.handleChange} /></div>
-              </div>
-              <div className='question'>
-                <div className='ask'><label>Do you feel energetic today?</label></div>
-                <div className='answer'>
-                 <input type="text" name='energy' value={this.state.energy} onChange={this.handleChange} /></div>
-              </div>
-              <div className='question'>
-                <div className='ask'>
-                  <label>How happy are you today?</label>
-                </div>
-                <div className='answer'>
-                  <input type="text" name='happiness' value={this.state.happiness} onChange={this.handleChange} />
-                </div>
-              </div>
-            </TabPane>
-            <TabPane tab="Emotion status" key="2">
-              <div className='question'>
-                <label>Did you sleep well last night? <input type="text" name='sleep' value={this.state.sleep} onChange={this.handleChange} /></label>
-              </div>
-              <div className='question'>
-                <label>Do you feel energetic? <input type="text" name='energy' value={this.state.energy} onChange={this.handleChange} /></label>
-              </div>
-            </TabPane>
-            <TabPane tab="Spitial status" key="3">
-              <div className='question'>
-                <div className='ask'>
-                  <label>Did you sleep well last night?</label>
-                </div>
-                <div className='answer'>
-                  <input type="text" name='sleep' value={this.state.sleep} onChange={this.handleChange} />
-                </div>
-              </div>
-              <div className='question'>
-                <label>Do you feel energetic? <input type="text" name='energy' value={this.state.energy} onChange={this.handleChange} /></label>
-              </div>
-            </TabPane>
-          </Tabs>
-          <div className='button'>
-            <input type="submit" value="Save" />
-            <input type="button" value="Never mind" onClick={this.handleSubmit} />
-          </div>
-        </form>
-      )
     }
 
     onTabsChangedHandler () {
@@ -172,7 +83,6 @@ define(['react', 'react-dom', 'antd'], function (React, ReactDOM, AntD) {
     }
 
     render () {
-      // return this.getHardCodedProfile()
       return this.getProfile()
     }
   }
